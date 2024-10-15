5802a53026990a9d62bb2a68b92f7f34aeb4b18b
adriley <adrileyfdev@gmail.com>
2024-10-15 01:22:25 -0300

    Initial commit

---
+++ src/main/java/com/example/appdocker/AppdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppdockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appdocker/AppdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
